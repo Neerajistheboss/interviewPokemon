73322ece662eb9cda02398ebeefae97cf96f9d5f
Neerajistheboss <neerajistheboss@gmail.com>
2021-11-16 16:58:07 +0530

    no API requests if final page is reached

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -5,7 +5,9 @@ import axios from 'axios'
 
 const HomePage=()=>{
     const [pokemonList,setPokemonList]=useState([])
-
+    let count    //count of pokemon on particular page of api call
+    let totalCount   // total count of pokemon in database
+    let totalPokemonRecieved=0  //total pokemon data recieved
     const [page,setPage]=useState(1)
     const [size,setSize]=useState(10)
     
@@ -13,7 +15,11 @@ const HomePage=()=>{
     //function to check whether bottom of page about to reach
     const checkBottom=()=>{
         if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-            setPage(page+1)  // bottom reached , increase page no. to fetch more data
+            // bottom reached , increase page no. to fetch more data
+            totalPokemonRecieved+=(page-1)*size+count
+            console.log(totalPokemonRecieved)
+            if(totalPokemonRecieved<totalCount) // more pokemon data can be fetched , increase page number and fetch data again
+            setPage(page+1)  
          }
     }
 
@@ -26,7 +32,12 @@ const HomePage=()=>{
 
     useEffect(()=>{
         axios.get(`https://api.pokemontcg.io/v2/cards?page=${page}&pageSize=${size}`)
-             .then(response=>setPokemonList([...pokemonList,...response.data.data]))
+             .then(response=>{
+                 setPokemonList([...pokemonList,...response.data.data])
+                 totalCount=response.data.totalCount  //total count of pokemon in database
+                 count=response.data.count
+                })
+                
     },[page,size]) // dependecy on page no. and size.
 
     return(
